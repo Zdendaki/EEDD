1fa5cfd96305b6d51641a622fabc18a3aa8948ce
Zdeněk Kundrát <zdendaki@zdendaki.net>
2025-03-01 23:15:04 +0100

    EVAL Train ViewModels

---
+++ EVAL/ViewModels/TrainRouteVM.cs
@@ -0,0 +1,42 @@
+﻿using Common.Data;
+
+namespace EVAL.ViewModels
+{
+    public class TrainRouteVM : ViewModel
+    {
+        public uint ID { get; init; }
+
+        private string name;
+        public string Name
+        {
+            get => name;
+            set
+            {
+                name = value;
+                OnPropertyChanged(nameof(Name));
+            }
+        }
+
+        private RouteTrack track;
+        public RouteTrack Track
+        {
+            get => track;
+            set
+            {
+                track = value;
+                OnPropertyChanged(nameof(Track));
+            }
+        }
+
+        private string comment;
+        public string Comment
+        {
+            get => comment;
+            set
+            {
+                comment = value;
+                OnPropertyChanged(nameof(Comment));
+            }
+        }
+    }
+}
